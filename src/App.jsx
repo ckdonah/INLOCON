707eba1f58f6c6df857ab9085412630e710ae24f
Godwin <138124430+ckdonah@users.noreply.github.com>
2025-06-29 18:49:24 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-700">
+        Tailwind 3.4.1 is worknnning!nnn
+      </h1>
+    </div>
+  );
+}
