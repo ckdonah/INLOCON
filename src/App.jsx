aef255befa65a2e5a8a043c4dab531a0a0590673
Godwin <138124430+ckdonah@users.noreply.github.com>
2025-06-30 10:01:23 +0200

    herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";  
+import React from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* Other components can be added here */}
+      <Hero />
     </div>
   );
-} 
+}
 export default App;
